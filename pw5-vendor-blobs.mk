5553e47c43fe5d82e2baab1dc2d9a962947df0ff
mysteryagr <ahmedrahmy842@gmail.com>
2017-06-21 08:17:20 +0530

    First commit

---
+++ pw5-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/pw5/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/pw5/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/pw5/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/pw5/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/pw5/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/gionee/pw5/vendor,system/vendor)
